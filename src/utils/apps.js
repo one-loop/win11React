e8b000043d3e823380d8a00eafc4938ee761acd2
Saad <78092430+one-loop@users.noreply.github.com>
2023-03-26 18:11:13 +0530

    Update apps.js

--- src/utils/apps.js
+++ src/utils/apps.js
@@ -129,7 +129,7 @@ const apps = [
     icon: "mail",
     type: "app",
     action: "EXTERNAL",
-    payload: "mailto:blueedgetechno@gmail.com",
+    payload: "mailto:ss17886@nyu.edu",
   },
   {
     name: "Movies",
